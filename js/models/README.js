9ec171a0946868789944d4f0a6a9ebd10ab5414b
Nicole Yaciuk <nyaciuk@resolute.com>
2013-07-02 16:59:11 -0400

    little bitty aditions to the index and readme

--- js/models/README.js
+++ js/models/README.js
@@ -1,4 +1,4 @@
 Questions about models.js
 
-1. Is buttontitle a valid attribute?
-2. Can I put the keys ie: title, buttontitle, and completed in quotes and it still be valid (backbone, JSON, etc)?
+1. Is buttontitle a valid attribute (or should I be calling it a value?)?
+2. Can I put the keys ie: title, buttontitle, and completed in quotes and it still be valid (backbone, JSON, etc)? Is that better?
