37767d3dd660678eaa19c736334a6ca7ce825351
Nicole Yaciuk <nyaciuk@resolute.com>
2013-07-02 16:24:33 -0400

    adding more q's and structuring index.html

--- js/views/view.js
+++ js/views/view.js
@@ -5,6 +5,7 @@ var PageView = Backbone.View.extend({
 	el: '.container',
 
 	// Cache the template function for a single item.
+	// Can I name _.template whatever I want? 
 	pageTemplate: _.template("An example template"),
 
 	events: {
